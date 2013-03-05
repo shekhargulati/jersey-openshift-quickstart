c3fdd7fb3d4c29ca82c87c681035c9a29b2b9960
Shekhar Gulati <shekhargulati84@gmail.com>
2013-03-05 14:08:38 +0530

    updated web.xml to use package to scan and removed JaxrsActivator

--- src/main/java/com/openshift/examples/JaxrsActivator.java
+++
@@ -1,9 +0,0 @@
-package com.openshift.examples;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class JaxrsActivator extends Application{
-
-}
